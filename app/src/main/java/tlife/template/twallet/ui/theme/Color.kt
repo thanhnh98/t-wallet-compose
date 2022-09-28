795b2f9b2beb3fe23db896a795a1518959e49ce8
Thanh <h.thanhnguyen@okxe.com>
2022-09-28 12:22:31 +0700

    int proj

---
+++ app/src/main/java/tlife/template/twallet/ui/theme/Color.kt
@@ -0,0 +1,39 @@
+package tlife.template.twallet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+object PrimaryColor {
+    val Blue = Color(0xff1F6CFF)
+    val Orange = Color(0xffff9900)
+}
+
+object SecondaryColor {
+    val DarkBlue = Color(0xff004AD7)
+    val OceanBlue = Color(0xff00d1ff)
+    val SoftRed = Color(0xffff6854)
+    val Purple = Color(0xff8674f5)
+    val Red = Color(0xffff4444)
+    val Green = Color(0xff17d85c)
+    val Black = Color(0xff000000)
+    val Gray1 = Color(0xff949494)
+    val Gray2 = Color(0xffb5b5b5)
+    val Gray3 = Color(0xffd7d7d7)
+    val Gray4 = Color(0xfff2f2f2)
+    val Gray5 = Color(0xfff9f9f9)
+}
+
+object ShadeColor {
+    val Blue2 = Color(0xff1f6cff)
+    val LightBlue = Color(0xffAECAFF)
+    val LightBlue2 = Color(0xffE9F0FF)
+    val LightGreen = Color(0xffE8FBEF)
+    val LightOrange = Color(0xffFFF7F0)
+}
